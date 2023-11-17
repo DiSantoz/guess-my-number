1bf8b11f38167227cbe8772d7cf031509d577360
DiSantoz <dferreira0191@gmail.com>
2023-11-17 15:45:48 -0500

    html edits, and event listner for ckeck

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-'use strict';
+"use strict";
+
+// click event handler for check button to confirm user selection
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
