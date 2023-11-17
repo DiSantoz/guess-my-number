0ea7ca179711191b5cf4251685439ca369ddc7d0
DiSantoz <dferreira0191@gmail.com>
2023-11-17 15:58:48 -0500

    logic for guess number and user err

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 "use strict";
+// random number 1 - 20, add + 1 to get 20
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
 
 // click event handler for check button to confirm user selection
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".guess").value);
+  // convert string value to number
+  const guess = Number(document.querySelector(".guess").value);
+
+  console.log(typeof guess, guess);
+
+  //   if no number selected, display message
+  if (!guess) {
+    const noNumber = document.querySelector(".message");
+
+    noNumber.textContent = "Please guess a number!";
+    noNumber.style.color = "red";
+  }
 });
