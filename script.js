97147e25f3d6480ce9bf0c45bf82879524311df8
DiSantoz <dferreira0191@gmail.com>
2023-11-17 16:22:10 -0500

    implement game over if 0 score reached

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 "use strict";
 // random number 1 - 20, add + 1 to get 20
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+console.log(secretNumber);
 
 // message
 const message = document.querySelector(".message");
+
+// default score
+let userScore = 20;
+
+// score message
+const scoreDisplay = document.querySelector(".score");
 
 // click event handler for check button to confirm user selection
 document.querySelector(".check").addEventListener("click", function () {
@@ -20,11 +26,22 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     message.textContent = "Winner, winner!";
     message.style.color = "green";
-  } else if (guess > secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+  } else if (guess > secretNumber && userScore > 1) {
     message.textContent = "A bit too high of a guess";
     message.style = "";
-  } else if (guess < secretNumber) {
+    // decrease score by 1
+    userScore--;
+    scoreDisplay.textContent = userScore;
+  } else if (guess < secretNumber && userScore > 1) {
     message.textContent = "You guessed too low";
     message.style = "";
+    // decrease score by 1
+    userScore--;
+    scoreDisplay.textContent = userScore;
+  } else {
+    message.textContent = "GAME OVER";
+    message.style.color = "red";
+    scoreDisplay.textContent = 0;
   }
 });
